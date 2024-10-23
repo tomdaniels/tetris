4ecd20f3b69ad59f745c0f47eefb60b647d27929
tdtele <thomsondaniels@hotmail.com>
2024-10-23 15:16:06 +1100

    o-block only has one rotation

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -39,9 +39,6 @@ public:
   OBlock() {
     id = 4;
     cells[0] = {Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)};
-    cells[1] = {Position(0, 0), Position(0, 2), Position(1, 0), Position(1, 1)};
-    cells[2] = {Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)};
-    cells[3] = {Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)};
   }
 };
 
