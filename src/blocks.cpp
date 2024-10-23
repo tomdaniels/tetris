4fc9c33a84316a2f4abab4b3a0d1906d6b7bc9f9
tdtele <thomsondaniels@hotmail.com>
2024-10-23 18:38:23 +1100

    fix the t block second rotation frame

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -64,7 +64,7 @@ public:
     id = 6;
     cells[0] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)};
     cells[1] = {Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)};
-    cells[2] = {Position(1, 1), Position(1, 1), Position(1, 2), Position(2, 1)};
+    cells[2] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)};
     cells[3] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)};
     move(0, 3);
   }
